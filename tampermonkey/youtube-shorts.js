72aa20991af8cefd60fa914074166538f765fd92
Mrweb <33089526+mrwebfr@users.noreply.github.com>
2022-10-07 13:32:44 +0200

    Create youtube-shorts.js

---
+++ tampermonkey/youtube-shorts.js
@@ -0,0 +1,56 @@
+// ==UserScript==
+// @name           Youtube Shorts Blocker
+// @author         Mrweb.fr
+// @namespace      https://www.anosher.com/
+// @version        1.0.0
+// @description    It Blocks Youtube Shorts
+// @match          https://www.youtube.com/*
+// @updateURL      https://raw.githubusercontent.com/mrwebfr/mw-scripts/main/tampermonkey/gmail.js
+// @downloadURL    https://raw.githubusercontent.com/mrwebfr/mw-scripts/main/tampermonkey/gmail.js
+// @license      MIT
+// ==/UserScript==
+/* jshint esversion: 6 */
+
+(function () {
+    "use strict";
+
+    // Your code here...
+
+    let delay = 1000; //1000 = 1 sec
+
+  //removes shorts from side panel
+  setTimeout(() => {
+    let element = document.querySelectorAll("#endpoint");
+    let nonExistentFirstElement = element[2];
+    if (nonExistentFirstElement) nonExistentFirstElement.remove();
+  }, delay * 2);
+
+  function removeShortsVideos() {
+    let output = [];
+    let regex = /\/shorts\/*/g;
+    for (let i of document.querySelectorAll("*")) {
+      if (regex.test(i.href)) {
+        output.push(i);
+        var bruh = i.parentElement;
+        bruh.parentElement.remove();
+      }
+    }
+  }
+
+  function redirectShorts() {
+    let shortsPlayer = document.getElementById("shorts-player");
+    let shortsPlayerParent;
+    if (shortsPlayer) shortsPlayerParent = shortsPlayer.parentElement;
+    if (shortsPlayerParent != undefined || null) { }
+  }
+
+  function removeShorts() {
+    removeShortsVideos();
+
+    redirectShorts();
+  }
+
+  var interval = setInterval(function () {
+    removeShorts();
+  }, delay);
+})();
