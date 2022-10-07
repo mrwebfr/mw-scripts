f6feec364e30dc75f53e393137975a9bf48010e3
Mrweb <33089526+mrwebfr@users.noreply.github.com>
2022-10-07 12:57:34 +0200

    Update gmail.js

--- tampermonkey/gmail.js
+++ tampermonkey/gmail.js
@@ -1,1 +1,37 @@
-
+// ==UserScript==
+// @name           Gmail POP3 Auto Check
+// @author         Mrweb.fr
+// @version        1.0.0
+// @description    Infinite loop that checks POP3 protocol mails
+// @license MIT
+// @match          https://mail.google.com/mail/u/0/*
+// @grant          none
+// ==/UserScript==
+(function () {
+    const text = 'Consulter votre messagerie maintenant'
+    const first = 5 // seconds
+    const interval = 300; // seconds
+    const debug = false;
+    function log(msg) { debug && console.log(msg); }
+    function checkPop3() {
+        const gmailWindow = window;
+        if (gmailWindow.location.hash.indexOf("#settings/accounts") != -1) {
+            const xpath = "//span[text()='"+text+"']";
+            const refreshAccounts = () => {
+                const selectedNodeElements = gmailWindow.document.evaluate(xpath, gmailWindow.document, null, XPathResult.ANY_TYPE, null);
+                let currentNode = selectedNodeElements.iterateNext();
+                if (currentNode === null) {
+                    setTimeout(refreshAccounts, 100);
+                } else {
+                    while (currentNode) {
+                        currentNode.click();
+                        currentNode = selectedNodeElements.iterateNext();
+                    };
+                };
+            };
+            setTimeout(refreshAccounts, 100);
+        }
+    }
+    setTimeout(checkPop3, first * 1000);
+    setInterval(checkPop3, interval * 1000);
+})();
