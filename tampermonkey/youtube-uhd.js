4705c807fef068c4b29004fd73d2c3d52a6c972c
Mrweb <33089526+mrwebfr@users.noreply.github.com>
2023-04-20 20:54:30 +0200

    Create youtube-uhd.js

---
+++ tampermonkey/youtube-uhd.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         YouTube Auto HD/4K
+// @namespace    https://www.mrweb.fr
+// @version      1
+// @description  Automatically play YouTube videos in UHD quality
+// @author       Ymrweb
+// @match        https://www.youtube.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Wait for the page to load
+    window.addEventListener('load', function() {
+        // Get the player element
+        var player = document.getElementById('movie_player');
+        if (!player) return;
+
+        // Set the preferred playback quality to the highest available
+        var availableQualities = player.getAvailableQualityLevels();
+        var highestQuality = availableQualities.indexOf('hd2160') >= 0 ? 'hd2160' : 'hd1080';
+        player.setPlaybackQualityRange(highestQuality);
+        player.setPlaybackQuality(highestQuality);
+
+        // Reload the video with the new quality setting
+        player.reload();
+    });
+})();
